0877196326a4a771f8358a23458e923ba6c10280
Bryant McDonnell <bemcdonnell@gmail.com>
2015-06-03 18:08:05 -0400

    exporting functions

--- swmm/modelIO.py
+++ swmm/modelIO.py
@@ -18,9 +18,51 @@ class MODELOBJECTS:
         
         #Hydrology Collections
         self.Options = OPTIONS()
+        
+    def Eval_Nodes(self):
         self.Nodes = NODES(self)
+        
+    def Eval_Hydrology(self):
         self.Hydrology = HYDROLOGY(self)
 
+    def export_Model(self):
+        
+        if hasattr(self, 'Nodes'):
+            print '[JUNCTIONS]'
+            for junc in self.Nodes.junctions.keys():
+                Node = self.Nodes.junctions[junc]
+                print '\t'.join([Node.NODENAME, str(Node.ELEVATION), str(Node.YMAX), str(Node.Y0), str(Node.YSURCHARGE), str(Node.PONDINGAREA)])
+
+        if hasattr(self.Nodes, 'coordinates'):
+            print '\n[COORDINATES]'
+            for node in self.Nodes.coordinates.keys():
+                Node = self.Nodes.coordinates[node]
+                print '\t'.join([Node.NODENAME, str(Node.xcoord), str(Node.ycoord)])
+
+        if hasattr(self,'Hydrology'):
+            print '\n[SUBCATCHMENTS]'
+            for scatch in self.Hydrology.subcatchments.keys():
+                SC = self.Hydrology.subcatchments[scatch]
+                print '\t'.join([str(SC.SUBCNAME),str(SC.RGAGE),str(SC.OUTID),str(SC.AREA),str(SC.PERCENT_IMPERV),\
+                                 str(SC.WIDTH),str(SC.SLOPE),str(SC.CLENGTH),str(SC.SNOWPACK)])
+
+            print '\n[SUBAREAS]'
+            for scatch in self.Hydrology.subareas.keys():
+                SA = self.Hydrology.subareas[scatch]
+                print '\t'.join([str(SA.SUBCNAME),str(SA.NIMP),str(SA.NPERV),str(SA.SIMP),\
+                                 str(SA.SPERV),str(SA.PERCENT_ZERO),str(SA.ROUTETO),str(SA.PERCENT_ROUTED)])                
+
+            print '\n[INFILTRATION]'
+            for scatch in self.Hydrology.infiltration.keys():
+                SC = self.Hydrology.infiltration[scatch]
+                print '\t'.join([str(SC.SUBCNAME),str(SC.SUCTION),str(SC.CONDUCT),str(SC.INITDEF)])
+
+        if hasattr(self.Hydrology, 'polygons'):
+            print '\n[POLYGONS]'
+            for scatch in self.Hydrology.polygons.keys():
+                SC = self.Hydrology.polygons[scatch]
+                for coord in SC.PolyCoord:
+                    print '\t'.join([str(SC.SUBCNAME), str(coord[0]), str(coord[1])])
 
     def add_OPTIONS(self,\
                     FLOW_UNITS = 'MGD',\
@@ -141,23 +183,11 @@ class HYDROLOGY:
     | 9 | 10| 11| 12|
      --- --- --- ---
      
-    1 - 
-    2 -
-    3 -
-    4 -
-    5 -
-    6 -
-    7 -
-    8 -
-    9 -
-    10-
-    11-
-    12-
+    1 -,2 -,3 -,4 -,5 -,6 -,7 -,8 -,9 -,10-,11-, 12-
     
     '''
     def __init__(sub, self):
-        #Options for building Infiltration Type
-        
+    
         sub.subcatchments = {}
         sub.subareas = {}
         sub.infiltration = {}
@@ -207,9 +237,7 @@ class HYDROLOGY:
         sub.subcatchments[SUBCNAME].SLOPE = SLOPE
         sub.subcatchments[SUBCNAME].CLENGTH = CLENGTH
         sub.subcatchments[SUBCNAME].SNOWPACK = SNOWPACK
-
-        
-        
+            
     def _add_subarea(sub, SUBCNAME, NIMP, NPERV, SIMP, SPERV,\
                      PERCENT_ZERO, ROUTETO, PERCENT_ROUTED):
         
@@ -254,9 +282,12 @@ class HYDROLOGY:
 if __name__ in '__main__':
     self = MODELOBJECTS()
     self.add_OPTIONS()
+    self.Eval_Nodes()
+    self.Eval_Hydrology()
+    
     self.Nodes.add_NODE('Node1',122,0,0)
-    self.Nodes.add_NODE('Node2',122,200,200)
+    self.Nodes.add_NODE('Node2',500,200,200)
     self.Hydrology.add_HYDROLOGY('A','Node1',222,'Node1',4000,50,200, gridpos=5)
     self.Hydrology.add_HYDROLOGY('B','Node2',44,'Node2',40100,520,2003)
 
-    
+    self.export_Model()
